84deda7d65d5ab8e4aa1014186d036231dc39c65
Disappear9 <Disappear9@users.noreply.github.com>
2020-04-28 20:13:16 +0800

    data/client_login should not have a trailing EOL (#5)
    Co-authored-by: ctaaronsan <ctaaronsan@users.noreply.github.com>

--- build/start.sh
+++ build/start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 if [ ! -f /hath/data/data/client_login ]; then
 	if [ $HatH_KEY ]; then
-		echo "${HatH_KEY}" > /hath/data/data/client_login
+		echo -n "${HatH_KEY}" > /hath/data/data/client_login
 	fi
 fi
 
-java -jar HentaiAtHome.jar $HatH_ARGS 
-
+java -jar HentaiAtHome.jar $HatH_ARGS
