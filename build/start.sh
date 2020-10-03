40cf9809b7a68fc33b840cce2027f909e51a8072
Disappear9 <Disappear9@users.noreply.github.com>
2020-10-03 22:16:01 +0800

    Update start.sh
    if user reset client key

--- build/start.sh
+++ build/start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-if [ ! -f /hath/data/data/client_login ]; then
-	if [ $HatH_KEY ]; then
+
+if [ $HatH_KEY ]
+	then
 		echo -n "${HatH_KEY}" > /hath/data/data/client_login
-	fi
+	else
+		echo "No HatH_KEY arg, exiting......"
+		exit 1
 fi
 
 java -jar HentaiAtHome.jar $HatH_ARGS
