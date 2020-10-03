ea3b9f242d8d8f5849812eae4fa56e2bfa75f92b
Disappear9 <Disappear9@users.noreply.github.com>
2020-10-04 01:37:08 +0800

    Update start.sh

--- build/start.sh
+++ build/start.sh
@@ -4,7 +4,8 @@ if [ $HatH_KEY ]
 	then
 		echo -n "${HatH_KEY}" > /hath/data/data/client_login
 	else
-		echo "No HatH_KEY arg, exiting......"
+		if [ ! -f /hath/data/data/client_login ]; then
+		echo "Login not found, try specify the HatH_KEY arg, exiting......"
 		exit 1
 fi
 
